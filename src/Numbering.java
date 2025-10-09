7f785a73a1e035efc492a3005b8dd64e0df11366
deatheed <95500649+deatheed@users.noreply.github.com>
2025-10-09 12:54:11 +0200

    Update Numbering.java

--- src/Numbering.java
+++ src/Numbering.java
@@ -1,16 +1,29 @@
+import java.util.ArrayList;
+
 public class Numbering{
-    private int[] d;
-    private int[] f;
+    private int[] d; // profondeur lors de la recherche
+    private int[] f; //date de fin ( après l'appel récursif)
     private int nb;
+    class Arc { int pdep; int pfin; }
+    private ArrayList<Arc> tf_a; // tree forward arc
+    private ArrayList<Arc> b_a; // back arc
+    private ArrayList<Arc> c_a; // cross arc
+    private String[] colors; 
+    
 
     public Numbering(int n){
         d = new int[n];
         f = new int[n];
+        tf_a = new ArrayList<Arc>();
+        b_a = new ArrayList<Arc>();
+        c_a = new ArrayList<Arc>();
+        colors = new String[n];
         nb = 0;
 
         for(int i=0; i<n;i++){
             d[i] = 0;
             f[i] = 0;
+            colors[i] = "noir";
         }
     }
     
@@ -32,6 +45,39 @@ public class Numbering{
         f[i] = val;
     }
 
+    public void add_ft_arc(int val1, int val2){
+        Arc a = new Arc();
+        a.pdep = val1;
+        a.pfin = val2;
+        tf_a.add(a);
+    }
+
+    public void add_b_arc(int val1, int val2){
+        Arc a = new Arc();
+        a.pdep = val1;
+        a.pfin = val2;
+        b_a.add(a);
+    }
+
+    public void add_c_arc(int val1, int val2){
+        Arc a = new Arc();
+        a.pdep = val1;
+        a.pfin = val2;
+        c_a.add(a);
+    }
+
+    public ArrayList<Arc> get_c_arc(){
+        return c_a;
+    }
+
+    public ArrayList<Arc> get_tf_arc(){
+        return tf_a;
+    }
+
+    public ArrayList<Arc> get_b_arc(){
+        return b_a;
+    }
+
     // NB
     public int getNB(){
         return nb;
@@ -39,6 +85,14 @@ public class Numbering{
 
     public void incrNB(){
         nb++;
+    }
+
+    public String[] getColors(){
+        return colors;
+    }
+
+    public void setColor(int indice, String color){
+        colors[indice] = color;
     }
 
     
